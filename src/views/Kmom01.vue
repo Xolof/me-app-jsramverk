92e40ddaa1a00d7c2db251af840dddc249064783
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2020-09-07 19:06:16 +0200

    Add link to GitHub repo

--- src/views/Kmom01.vue
+++ src/views/Kmom01.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="kmom01">
 
+    <h1>GitHub</h1>
+
+    <a href="https://github.com/Xolof/me-app-jsramverk">Repo on GitHub</a>
+
     <h1>README.md för "me-app"</h1>
 
     <h2>Installera nödvändiga NPM-moduler med</h2>
