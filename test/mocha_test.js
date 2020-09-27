653a80dc3935943607df9c92804cfa7410c77979
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2020-09-27 19:13:47 +0200

    uncomment first test

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -61,21 +61,21 @@ test.describe("me-app", function() {
 
     // Test case
     // it eller test.it
-    // it("Test index", function(done) {
-    //     this.timeout(20000);
-    //
-    //     browser.getTitle().then(function(title) {
-    //         assert.equal(title, "me-app");
-    //     })
-    //     // .then(done, done);
-    //     // .catch(done);
-    //
-    //     // matchUrl("/#/");
-    //     //
-    //     // browser.findElement(By.className("me-img"));
-    //
-    //     done();
-    // });
+    it("Test index", function(done) {
+        this.timeout(20000);
+
+        browser.getTitle().then(function(title) {
+            assert.equal(title, "me-app");
+        })
+        // .then(done, done);
+        // .catch(done);
+
+        // matchUrl("/#/");
+        //
+        // browser.findElement(By.className("me-img"));
+
+        done();
+    });
 
     // test.it("Test go to Redovisning", function(done) {
     //     goToNavLink("Redovisning");
